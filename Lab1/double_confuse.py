f5c3cf4692bdc5c0b35285597d9c0d856e887b5c
kostya <19bone96@gmail.com>
2017-03-01 08:47:18 +0300

    Refactored output and main function structure

--- Lab1/double_confuse.py
+++ Lab1/double_confuse.py
@@ -14,17 +14,20 @@ def mc_loren_summary(num, iterates):
 
 
 class Complex:
+    mc_loren_accuracy = 10
     number = 0
     savedOne = False
     lowerOne = False
 
-    def __init__(self, number):
+    def __init__(self, number, mc_loren_accuracy=10):
         self.number = number
         if self.number < 0:
             self.number *= (-1)
         if self.number < 1:
             self.lowerOne = True
             self.number = 2 - self.number
+
+        self.mc_loren_accuracy = mc_loren_accuracy
 
     def sqr(self):
         if self.savedOne:
@@ -46,7 +49,7 @@ class Complex:
                 self.savedOne = True
                 self.number -= 1
             module = self.number
-            beta = mc_loren_summary(module, 10)
+            beta = mc_loren_summary(module, self.mc_loren_accuracy)
             self.number = beta
         else:
             self.number = math.sqrt(self.number)
@@ -55,26 +58,36 @@ class Complex:
         for i in range(n):
             self.sqrt()
 
+    def magic_function(self, iterates, out):
+        self.sqrt_n(iterates)
+        if out:
+            print(self)
+        self.sqr_n(iterates)
+
     def __str__(self):
         if self.lowerOne:
             if self.savedOne:
-                return str.replace(str(2 - self.number), "1.", "0.")
+                if self.number > 0.0000000001:
+                    return str(1-self.number)
+                else:
+                    return "1 - " + str(self.number)
             else:
                 return str(2 - self.number)
         else:
             if self.savedOne:
-                return str.replace(str(self.number), "0.", "1.")
+                return "1 + " + str(self.number)
             else:
                 return str(self.number)
 
-
-def main(num, iterates=100):
-    x = Complex(num)
+""" num is number to work,
+    iterates is number of repeatings for sqrt and sqr
+    mc_loren_accuracy is amount of members in mcLoren summary
+"""
+def main(num, iterates=100, mc_loren_accuracy=10):
+    x = Complex(num, mc_loren_accuracy)
     print("sqrt and sqr " + str(iterates) + " times:")
     print(x)
-    x.sqrt_n(iterates)
-    x.sqr_n(iterates)
+    x.magic_function(iterates, True)
     print(x)
 
-
-main(5, 100)
+main(0.1, 100, 20)
